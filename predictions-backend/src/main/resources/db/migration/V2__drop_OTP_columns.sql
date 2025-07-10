c76a592533eb45106ec36427554d957af92c60d7
ogkomori <majorogkomori@gmail.com>
2025-07-10 20:57:05 +0100

    Implemented Flyway DB migration

---
+++ predictions-backend/src/main/resources/db/migration/V2__drop_OTP_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE user_entity DROP COLUMN verifyotp;
+ALTER TABLE user_entity DROP COLUMN verifyotpexpire_at;
+ALTER TABLE user_entity DROP COLUMN resetotp;
+ALTER TABLE user_entity DROP COLUMN resetotpexpire_at;
