b45be87e5d65cacdd18c7acf08e71845f3491810
ogkomori <majorogkomori@gmail.com>
2025-08-26 21:37:33 +0100

    Removed unecessary logging

--- predictions-backend/src/main/java/com/komori/predictions/exception/GlobalExceptionHandler.java
+++ predictions-backend/src/main/java/com/komori/predictions/exception/GlobalExceptionHandler.java
@@ -17,77 +17,77 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AccountNotVerifiedException.class)
-    public ResponseEntity<?> handleAccountNotVerified(AccountNotVerifiedException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "Account not verified");
+    public ResponseEntity<?> handleAccountNotVerified() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Account not verified");
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<?> handleEmailAlreadyExists(EmailAlreadyExistsException e) {
-        return buildResponse(e, HttpStatus.CONFLICT, "Email already exists");
+    public ResponseEntity<?> handleEmailAlreadyExists() {
+        return buildResponse(HttpStatus.CONFLICT, "Email already exists");
     }
 
     @ExceptionHandler(OtpExpiredException.class)
-    public ResponseEntity<?> handleOtpExpired(OtpExpiredException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "OTP expired");
+    public ResponseEntity<?> handleOtpExpired() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "OTP expired");
     }
 
     @ExceptionHandler(OtpIncorrectException.class)
-    public ResponseEntity<?> handleOtpIncorrect(OtpIncorrectException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "OTP incorrect");
+    public ResponseEntity<?> handleOtpIncorrect() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "OTP incorrect");
     }
 
     @ExceptionHandler(OtpNotFoundException.class)
-    public ResponseEntity<?> handleOtpNotFound(OtpNotFoundException e) {
-        return buildResponse(e, HttpStatus.NOT_FOUND, "OTP not found for user");
+    public ResponseEntity<?> handleOtpNotFound() {
+        return buildResponse(HttpStatus.NOT_FOUND, "OTP not found for user");
     }
 
     @ExceptionHandler(MailException.class)
     public ResponseEntity<?> handleMailException(MailException e) {
-        return buildResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<?> handleBadCreds(BadCredentialsException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "Email or password incorrect");
+    public ResponseEntity<?> handleBadCreds() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Email or password incorrect");
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<?> handleUsernameNotFound(UsernameNotFoundException e) {
-        return buildResponse(e, HttpStatus.NOT_FOUND, e.getMessage());
+        return buildResponse(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
-    public ResponseEntity<?> handlePasswordMismatch(PasswordMismatchException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "Password is incorrect");
+    public ResponseEntity<?> handlePasswordMismatch() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Password is incorrect");
     }
 
     @ExceptionHandler(LeagueNotFoundException.class)
-    public ResponseEntity<?> handleLeagueNotFound(LeagueNotFoundException e) {
-        return buildResponse(e, HttpStatus.NOT_FOUND, "League not found");
+    public ResponseEntity<?> handleLeagueNotFound() {
+        return buildResponse(HttpStatus.NOT_FOUND, "League not found");
     }
 
     @ExceptionHandler(IncorrectLeagueCodeException.class)
-    public ResponseEntity<?> handleIncorrectLeague(IncorrectLeagueCodeException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "Incorrect league code");
+    public ResponseEntity<?> handleIncorrectLeague() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Incorrect league code");
     }
 
     @ExceptionHandler(PublicityMismatchException.class)
-    public ResponseEntity<?> handlePublicityMismatch(PublicityMismatchException e) {
-        return buildResponse(e, HttpStatus.BAD_REQUEST, "Publicity mismatch");
+    public ResponseEntity<?> handlePublicityMismatch() {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Publicity mismatch");
     }
 
     @ExceptionHandler(LeagueAlreadyJoinedException.class)
-    public ResponseEntity<?> handleLeagueAlreadyJoined(LeagueAlreadyJoinedException e) {
-        return buildResponse(e, HttpStatus.CONFLICT, "League already joined");
+    public ResponseEntity<?> handleLeagueAlreadyJoined() {
+        return buildResponse(HttpStatus.CONFLICT, "League already joined");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneralException(Exception e) {
-        return buildResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
+        log.error("Unexpected error occurred: ", e);
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
     }
 
-    private ResponseEntity<?> buildResponse(Exception e, HttpStatus status, String message) {
-        log.error("Error occurred: ", e);
+    private ResponseEntity<?> buildResponse(HttpStatus status, String message) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", status.value());
