e8169e61afa3071389bdb03a53ca6c41a1d4ccd4
Tega Komori <majorogkomori@gmail.com>
2026-02-13 14:46:59 +0000

    Refactor matches table: remove old fixture ID and rename column

--- predictions-backend/src/main/resources/db/migration/V21__remove_old_fixture_id_from_matches.sql
+++ predictions-backend/src/main/resources/db/migration/V21__remove_old_fixture_id_from_matches.sql
@@ -2,5 +2,7 @@ TRUNCATE TABLE players;
 
 ALTER TABLE matches
     DROP COLUMN match_id,
-    ALTER COLUMN old_fixture_id TYPE BIGINT,
+    ALTER COLUMN old_fixture_id TYPE BIGINT;
+
+ALTER TABLE matches
     RENAME COLUMN old_fixture_id TO match_id;
