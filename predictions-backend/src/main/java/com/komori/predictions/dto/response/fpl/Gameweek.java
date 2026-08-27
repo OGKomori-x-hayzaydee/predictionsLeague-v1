0d0ffec3f6347bfb89ee8f49d2a63ab98080cf5b
Tega Komori <majorogkomori@gmail.com>
2026-08-27 16:04:11 +0100

    Add NoArgsConstructor to Gameweek and IndividualGameweek classes

--- predictions-backend/src/main/java/com/komori/predictions/dto/response/fpl/Gameweek.java
+++ predictions-backend/src/main/java/com/komori/predictions/dto/response/fpl/Gameweek.java
@@ -3,17 +3,20 @@ package com.komori.predictions.dto.response.fpl;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Gameweek {
     private List<IndividualGameweek> events;
 
     @Data
     @AllArgsConstructor
+    @NoArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class IndividualGameweek {
         private Integer id;
